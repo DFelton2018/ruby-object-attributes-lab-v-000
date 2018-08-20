7743031dc2edf82937c9707760a7336d7c8f46dd
Duane Felton <dfelton2010@gmail.com>
2018-08-20 15:56:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,12 +7,11 @@ end
 def name
   "#{@name}".strip
   
-  def breed=(breed)
-    @breed = breed
-  end
+def breed=(breed)
+  @breed = breed
+end
   
-  def breed
-    "#{@breed}".strip
-
+def breed
+  "#{@breed}".strip
 end
 end
